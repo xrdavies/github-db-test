7706feba8f96c252a8cb3ff2633f8d4bc20a6c6f
Frozen <1884084+xrdavies@users.noreply.github.com>
2022-03-20 02:49:01 +0800

    add test

---
+++ index.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { GithubDB } from "github-db";
+
+let db = new GithubDB({ token: process.env.PERSONAL_TOKEN, owner: 'xrdavies', repo: 'github-db', branch: 'db' });
+
+console.log(await db.get('test2.json'));
+
+console.log(await db.add({ d: 1 }));
+
+console.log(await db.update("test2.json", { d: 3 }));
+
+// db.remove("99d26c869671a2412368e664a19568d4.json", (err, res) => {
+//     console.log(err);
+//     console.log(res);
+// })
+
+console.log(await db.list());
