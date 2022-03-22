6d477b493b6610e4424bc39c9743ab525495130d
Frozen <1884084+xrdavies@users.noreply.github.com>
2022-03-23 04:04:01 +0800

    update to new version of github-keyvalue

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import 'dotenv/config';
 import { GithubKV } from "github-keyvalue";
-
 let db = new GithubKV({ token: process.env.PERSONAL_TOKEN, owner: 'xrdavies', repo: 'github-keyvalue', branch: 'db' });
 
 // console.log(await db.get('test2.json'));
-
 // console.log(await db.add({ d: 1 }));
-
 // console.log(await db.update("test2.json", { d: 3 }));
 
 // db.remove("99d26c869671a2412368e664a19568d4.json", (err, res) => {
@@ -15,3 +12,8 @@ let db = new GithubKV({ token: process.env.PERSONAL_TOKEN, owner: 'xrdavies', re
 // })
 
 console.log(await db.list());
+
+// import { Octokit } from "@octokit/rest";
+// let octokit = new Octokit({ auth: process.env.PERSONAL_TOKEN });
+// let {data} = await octokit.rest.repos.getContent({ owner: 'xrdavies', path: undefined, repo: 'github-keyvalue', ref: 'db' });
+// console.log(data);
